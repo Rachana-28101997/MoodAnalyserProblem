6bd0815ae42809b42b60b3a7ad20ce8ddcdc6a4e
Rachana <rachanarrachu28@gmail.com>
2020-10-28 02:34:35 +0530

    Added : Analysing the mood Happy or Sad

---
+++ src/test/java/com/moodanalyserproblem/MoodAnalyser.java
@@ -0,0 +1,12 @@
+package com.moodanalyserproblem;
+
+	public class MoodAnalyser {
+
+		public String analyseability(String message) {
+			if (message.contains("Sad"))
+				return "SAD";
+			return "HAPPY";
+		}
+	}
+
+
